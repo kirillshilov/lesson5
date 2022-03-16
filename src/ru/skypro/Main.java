856d894cfd22091e44df0cac8a73befb44582227
kirillshilov <sarataza@inbox.ru>
2022-03-16 17:30:51 +0300

    commit

--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -1,5 +1,7 @@
 package ru.skypro;
 
+import java.sql.SQLOutput;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -14,35 +16,62 @@ public class Main {
             int[] num = new int[]{1, 2, 3};
             float[] numTwo = new float[]{1.57f, 7.654f, 9.986f};
             int[] numThree = {1, 15, 48};
-            System.out.print(num [0] + ",");
-            System.out.print(num [1] + ",");
-            System.out.println(num [2]);
-            System.out.print(numTwo [0] + ",");
-            System.out.print(numTwo [1] + ",");
-            System.out.println(numTwo [2]);
-            System.out.print(numThree [0] + ",");
-            System.out.print(numThree [1] + ",");
-            System.out.println(numThree [2]);
+            for (int i = 0; i < num.length; i++) {
+                if (i == num.length - 1){
+                    System.out.println(num [i]);
+                    break;
+                }
+                System.out.print(num [i] + ", ");
+            }
+            for (int i = 0; i < numTwo.length; i++) {
+                if (i == num.length - 1){
+                    System.out.println(numTwo [i]);
+                    break;
+                }
+                System.out.print(numTwo [i] + ", ");
+            }
+            for (int i = 0; i < numThree.length; i++) {
+                if (i == num.length - 1){
+                    System.out.println(numThree [i]);
+                    break;
+                }
+                System.out.print(numThree [i] + ", ");
+            }
         }
         {
             // Задача 3
             int[] num = new int[]{1, 2, 3};
             float[] numTwo = new float[]{1.57f, 7.654f, 9.986f};
             int[] numThree = {1, 15, 48};
-            System.out.print(num [2] + ",");
-            System.out.print(num [1] + ",");
-            System.out.println(num [0]);
-            System.out.print(numTwo [2] + ",");
-            System.out.print(numTwo [1] + ",");
-            System.out.println(numTwo [0]);
-            System.out.print(numThree [2] + ",");
-            System.out.print(numThree [1] + ",");
-            System.out.println(numThree [0]);
+            for (int i = numTwo.length - 1; i >= 0; ) {
+                if (i == 0){
+                    System.out.println(num [i]);
+                    break;
+                }
+                System.out.print(num [i] + ",");
+                i = i-1;
+            }
+            for (int i = numTwo.length - 1; i >= 0; ) {
+                if (i == 0){
+                    System.out.println(numTwo [i]);
+                    break;
+                }
+                System.out.print(numTwo [i] + ",");
+                i = i-1;
+            }
+            for (int i = numThree.length - 1; i >= 0; ) {
+                if (i == 0){
+                    System.out.println(numThree [i]);
+                    break;
+                }
+                System.out.print(numThree [i] + ",");
+                i = i-1;
+            }
         }
         {
             // Задача 4
             int[] num = new int[]{1, 2, 3};
-            for (int i = 0; i < 3; i++) {
+            for (int i = 0; i < num.length; i++) {
                 int number = num [i];
                 if ( number % 2 != 0){
                     num [i] = number + 1;
